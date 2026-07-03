d45b2a0eaa1531229adfe2012bc1d96b7e57455f
richcande1-rca <richcande1@gmail.com>
2026-07-03 06:02:19 -0500

    Bump title build stamp

--- title-screen.js
+++ title-screen.js
@@ -3,7 +3,7 @@
   titleScreen.className = "title-screen";
   titleScreen.setAttribute("aria-label", "Opening title screen");
 
-  const orbitBuildStamp = "Orbit build 2026.07.03.2";
+  const orbitBuildStamp = "Orbit build 2026.07.03.3";
 
   const skipButton = document.createElement("button");
   skipButton.className = "title-skip";
